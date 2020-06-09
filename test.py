a4eb4bb4f4f255c827a72ef3670fc253105a2c1b
Beelord <martin23512@gmail.com>
2020-06-09 18:00:32 +0200

    AAAAAAAAAAA

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
+positive = "YES"
+negative = "NO"
+
+agree = "AAAAAAAA"
+disagree = "Nah"
+
 print("Waddup")
+print("Do yo kno tha wae?")
+verify = input("YES/NO: ")
+
+if verify = positive:
+    print(agree)
+else:
+    print(disagree)
